ac5db31ffbe4e0da9af7f924ccf06fb972f87a3d
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-10-22 21:15:30 -0600

    Añadiendo resto de componentes del navbar

--- Proyecto/frontend/src/app/app-routing.module.ts
+++ Proyecto/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './landingPage/home/home.component';
+import { VendeComponent } from './landingPage/vende/vende.component';
+import { CompraComponent } from './landingPage/compra/compra.component';
+import { LoginComponent } from './landingPage/login/login.component';
+import { RegisterComponent } from './landingPage/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'vende', component: VendeComponent},
+  {path:'compra', component: CompraComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Proyecto/frontend/src/app/app.module.ts
+++ Proyecto/frontend/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './landing-page/home/home.component';
-import { VendeComponent } from './landing-page/vende/vende.component';
+import { HomeComponent } from './landingPage/home/home.component';
+import { VendeComponent } from './landingPage/vende/vende.component';
+import { CompraComponent } from './landingPage/compra/compra.component';
+import { LoginComponent } from './landingPage/login/login.component';
+import { RegisterComponent } from './landingPage/register/register.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+/*import { RouterModule } from '@angular/router'; */
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    VendeComponent
+    VendeComponent,
+    CompraComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
