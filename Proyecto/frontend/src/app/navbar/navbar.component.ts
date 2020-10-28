bc7fb5311354ef85ded45698def8052e6ac9a701
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-10-28 13:52:56 -0600

    ++ compras y collapse de navbar

--- Proyecto/frontend/src/app/navbar/navbar.component.ts
+++ Proyecto/frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  public isCollapsed = true;
+  faBars=faBars;
   constructor() { }
 
   ngOnInit(): void {
