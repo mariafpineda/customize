4921cda5ca3e96f2e17a09f7bedb7aed5565f93d
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-11-08 23:56:09 -0600

    BACKEND
    REST API planes complete

--- Proyecto/backend/routers/planes-router.js
+++ Proyecto/backend/routers/planes-router.js
@@ -7,22 +7,87 @@ var mongoose = require('mongoose');
     router.post('/', function (req, res) {
         let plan = new planes(
             {
-                
+                nombrePlan : req.body.nombre,
+                cantidadPaginas: req.body.paginas,
+                cantidadProductos : req.body.productos,
+                precioPlan : req.body.precio
             }
-        )
+        );
+        plan.save().then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        });
     })
 
 //Read plans
+    router.get('/', function(req, res){
+        planes.find({}, {})
+        .then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        })
+    })
 
 //Update plan
+    router.put('/:idPlan', function(req, res){
+        planes.update(
+            {
+                _id: req.params.idPlan
+            },
+            {
+                nombrePlan : req.body.nombre,
+                cantidadPaginas: req.body.paginas,
+                cantidadProductos : req.body.productos,
+                precioPlan : req.body.precio
+            }
+        ).then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        })
+    })
 
 
 //Delete plan
+    router.delete('/:idPlan', function(req, res){
+        planes.remove(
+            {
+                _id: req.params.idPlan
+            }
+        ).then(result=>{
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        })
+    })
 
 
 // --------- //
 
 //Get plan
+    router.get('/:idPlan', function(req, res){
+        planes.find(
+            {
+                _id: req.params.idPlan
+            }
+        ).then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        })
+    })
 
 
 module.exports = router;
