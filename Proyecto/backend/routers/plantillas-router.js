4cc8763104736bc35d2be93b9a22b579d640c787
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-11-10 19:09:16 -0600

    BACKEND. REST API plantillas completo

--- Proyecto/backend/routers/plantillas-router.js
+++ Proyecto/backend/routers/plantillas-router.js
@@ -34,7 +34,75 @@ var plantillas = require('../models/plantillas');
     });
 
 //Update template
+    router.put('/:idTemplate', function(req, res){
+        plantillas.update(
+            {
+                _id: req.params.idTemplate
+            },
+            {
+                tituloTema : req.body.titulo,
+                descripcion : req.body.descripcion,
+                imagenes: JSON.parse(req.body.imagenes),
+                codigo : req.body.codigo
+            }
+        ).then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        });
+    })
 
 //Delete template
+    router.delete('/:idTemplate', function(req, res){
+        plantillas.remove(
+            {
+                _id: req.params.idTemplate
+            }
+        ).then(result =>{
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        });
+    });
+
+// ------- //
+
+//Get template
+    router.get('/:idTemplate', function(req, res){
+        plantillas.find(
+            {
+                _id: req.params.idTemplate
+            }
+        ).then(result => {
+            res.send(result);
+            res.end();
+        }).catch(error => {
+            res.send(error);
+            res.end();
+        })
+    })
+
+//Update images on template
+router.put('/:idTemplate/imagenes', function(req, res){
+    plantillas.update(
+        {
+            _id: req.params.idTemplate
+        },
+        {
+            imagenes: JSON.parse(req.body.imagenes)
+        }
+    ).then(result => {
+        res.send(result);
+        res.end();
+    }).catch(error => {
+        res.send(error);
+        res.end();
+    });
+})
+
 
 module.exports = router;
