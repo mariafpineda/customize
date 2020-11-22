8aec4fe5aea29c0fe075236e5c9f2d0be8431d30
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-11-22 14:03:14 -0600

    Sección de planes del lado del admin en proceso

--- Proyecto/frontend/src/app/admins/planes/planes.component.ts
+++ Proyecto/frontend/src/app/admins/planes/planes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PlanesService } from 'src/app/services/planes.service';
 
 @Component({
   selector: 'app-planes',
@@ -9,14 +10,25 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PlanesComponent implements OnInit {
   faPlus=faPlus;
+  faTrashAlt=faTrashAlt;
 
-  constructor(private modalService:NgbModal) { }
+  planes:any;
+  planSeleccionado:String;
+
+  constructor(private modalService:NgbModal,
+    private planesService:PlanesService) { }
 
   ngOnInit(): void {
+    this.planesService.getPlans()
+    .subscribe(res => {
+      this.planes= res;
+    }, error => console.log(error)
+    )
   }
 
   open(content, id){
     this.modalService.open(content, {centered: true});
+    this.planSeleccionado=id;
   }
   
 }
