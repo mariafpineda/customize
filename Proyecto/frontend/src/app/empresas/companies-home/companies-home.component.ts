ed806da783456ca0caae50b51125c563b097879b
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-12-05 16:40:01 -0600

    Navbar y sidebar de sección home en empresas completado

--- Proyecto/frontend/src/app/empresas/companies-home/companies-home.component.ts
+++ Proyecto/frontend/src/app/empresas/companies-home/companies-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faCog, faSignOutAlt, faTag, faList, faStore} from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle as farUserCircle, faFile as farFile } from "@fortawesome/free-regular-svg-icons"
 
 @Component({
   selector: 'app-companies-home',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies-home.component.css']
 })
 export class CompaniesHomeComponent implements OnInit {
+  
+  public isMenuCollapsed=true;
+  public isSidebarCollapsed=true;
+ /*Fontawesome*/
+  faCog=faCog;
+  faSignOutAlt=faSignOutAlt;
+  farUser=farUserCircle;
+  farFile=farFile;
+  faTag=faTag;
+  faList=faList;
+  faStore=faStore;
 
   constructor() { }
 
