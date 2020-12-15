0dd0483a6a1ed13cf10796674a8a699eaedd04db
mariafpineda <47511303+mariafpineda@users.noreply.github.com>
2020-12-14 22:11:57 -0600

    Iniciando sección de clientes

--- Proyecto/frontend/src/app/usuarios/users-home/users-home.component.ts
+++ Proyecto/frontend/src/app/usuarios/users-home/users-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { faCog, faShoppingCart, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { EmpresasService } from 'src/app/services/empresas.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-users-home',
@@ -6,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-home.component.css']
 })
 export class UsersHomeComponent implements OnInit {
+  faShoppingCart=faShoppingCart;
+  faSignOutAlt=faSignOutAlt;
 
-  constructor() { }
+  isMenuCollapsed=true;
+
+  empresas:any=[];
+  productos:any=[];
+  compras:any=[];
+
+  regionVisible="empresas";
+
+  constructor(private empresasService:EmpresasService,
+    private usuariosService:UsuariosService,
+    private route:Router) { }
 
   ngOnInit(): void {
+
+    this.empresasService.getCompanies()
+    .subscribe(res => {
+      this.empresas=res;
+      console.log(this.empresas)
+    }, error => console.log(error));
+
+  }
+
+  verProductos(id){
+    this.regionVisible="productos";
+    console.log(id);
+    this.obtenerProductos(id);
+  }
+
+  obtenerProductos(id){
+    this.empresasService.getProducts(id)
+    .subscribe(res => {
+      console.log(res[0].productos);
+      this.productos=res[0].productos;
+    },
+    error => {
+      console.log(error);
+    })
+  }
+
+  logout(){
+    localStorage.removeItem('idUser');
+    localStorage.removeItem('token');
+    this.route.navigate(['/']);
+  }
+
+  agregarCarrito(producto){
+    var carrito;
+    console.log(producto);
+    if(localStorage.getItem('carrito')==null){
+      localStorage.setItem('carrito', producto);
+    } else {
+      carrito=localStorage.getItem('carrito');
+      carrito.push(producto);
+      localStorage.setItem('carrito', carrito);
+    }
   }
 
 }
